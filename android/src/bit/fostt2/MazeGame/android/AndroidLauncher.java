d679ac33ecd19a6b266141743acec2de5d6b4a37
Lateralus31 <11fostth@gmail.com>
2015-05-21 10:54:14 +1200

    First Push
    Trying new engine

---
+++ android/src/bit/fostt2/MazeGame/android/AndroidLauncher.java
@@ -0,0 +1,16 @@
+package bit.fostt2.MazeGame.android;
+
+import android.os.Bundle;
+
+import com.badlogic.gdx.backends.android.AndroidApplication;
+import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
+import bit.fostt2.MazeGame.GameMain;
+
+public class AndroidLauncher extends AndroidApplication {
+	@Override
+	protected void onCreate (Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
+		initialize(new GameMain(), config);
+	}
+}
